8a4efde493af7b22929616c9adce122075b30f55
NonameBlank007 <nonameblank007@gmail.com>
2025-07-01 20:10:00 +0530

    Blaze: Script: Fix: Uinstall not working
    Signed-off-by: NonameBlank007 <nonameblank007@gmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,18 @@
-# Don't modify anything after this
-rm -rf /data/adb/service.d/BlazeBoost.sh
+#!/system/bin/sh
+
+# Force termination
+pkill -9 -f "BlazeBoost.sh" 2>/dev/null
+pkill -9 -f "custom.sh" 2>/dev/null
+
+# Remove lockfiles
+rm -f /data/adb/blazeboost.lock
+
+# Service script removal
+rm -f "/data/adb/service.d/BlazeBoost.sh"
+
+# Zombie process cleanup
+for pid in $(pgrep -f "BlazeBoost.sh"); do
+    kill -9 $pid 2>/dev/null
+done
 
 exit 0
