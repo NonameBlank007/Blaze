7d3b84433d5aecea4c161cb9bd41335860342b02
NonameBlank007 <nonameblank007@gmail.com>
2026-05-23 17:10:00 +0530

    Blaze: Script: Fix: prevent instant close of action dialouge in ksu & forks
    Signed-off-by: NonameBlank007 <nonameblank007@gmail.com>

--- common/action.sh
+++ common/action.sh
@@ -9,3 +9,15 @@ if ! sh $MODDIR/custom.sh; then
 fi
 
 echo -e "Mode change complete!"
+
+# warn and automatically closes dilogue if successful
+KSU_ACTIVE=false
+
+if [ "$KSU" = "true" ] || [ "$APATCH" = "true" ]; then
+    KSU_ACTIVE=true
+fi
+
+if $KSU_ACTIVE && [ "$KSU_NEXT" != "true" ] && [ "$WKSU" != "true" ] && [ "$MMRL" != "true" ]; then
+    echo -e "\nClosing dialog in 5 seconds..."
+    sleep 5
+fi
